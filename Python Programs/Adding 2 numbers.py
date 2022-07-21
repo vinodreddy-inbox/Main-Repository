5bc4441d9eeeaaead97a3d62bd28363b0984b4f7
vinodreddy-inbox <108283099+vinodreddy-inbox@users.noreply.github.com>
2022-07-22 00:34:53 +0530

    Created a Py program - Adding 2 numbers
    Practicing Python Programming :)

---
+++ Python Programs/Adding 2 numbers.py
@@ -0,0 +1,13 @@
+# This program adds two numbers
+
+print('Hi, enter the first number')
+num1 = input()
+print('Now, enter the second number')
+num2 = input()
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2} \nHave a nice day :)'.format(num1, num2, sum))
+
