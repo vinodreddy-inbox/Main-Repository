e589430d47adafd928ace72c1473e6741a50bb1f
vinodreddy-inbox <108283099+vinodreddy-inbox@users.noreply.github.com>
2022-07-22 19:22:19 +0530

    Create zip code finder.py

---
+++ Python Programs/zip code finder.py
@@ -0,0 +1,5 @@
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="geoapiExercises")
+place=input("Enter City Name: ")
+location = geolocator.geocode(place)
+print("Result: ", location)
