5b7a947461dc002c41972206862d8e220834cb4a
vinodreddy-inbox <108283099+vinodreddy-inbox@users.noreply.github.com>
2022-07-30 19:11:51 +0530

    Sample Py project on AWS

---
+++ AWS Projects/Test_WebAPP_Python/config.py
@@ -0,0 +1,7 @@
+#This project is from Intellipaat.
+customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
+customuser = "intellipaat"
+custompass = "intellipaat123"
+customdb = "employee"
+custombucket = "addemployee"
+customregion = "us-east-2"
